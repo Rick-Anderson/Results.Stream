5fd2b415bacee25f593a0bd37963725a94f923e2
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-07-07 12:33:48 -1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Processing;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/", async (HttpContext http) =>   // process-image
+{
+    using var image = await Image.LoadAsync("wwwroot/front.png");
+    int width = image.Width / 2;
+    int height = image.Height / 2;
+    image.Mutate(x => x.Resize(width, height));
+    http.Response.Headers.CacheControl = $"public,max-age={TimeSpan.FromHours(24).TotalSeconds}";
+    return Results.Stream(stream => image.SaveAsync(stream, PngFormat.Instance), "image/png");
+});
+
+app.Run();
