c00b73141365fd2514f5101b816d2aefe81d675c
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-07-07 13:10:20 -1000

    comments

--- Program.cs
+++ Program.cs
@@ -5,15 +5,19 @@ using SixLabors.ImageSharp.Processing;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", async (HttpContext http) =>   // process-image
+app.MapGet("/", async (HttpContext http) => 
 {
-    using var image = await Image.LoadAsync("wwwroot/front.png");
+    // removing following // produces ObjectDisposedException
+    // using
+    var image = await Image.LoadAsync("wwwroot/front.png");
     int width = image.Width / 2;
     int height = image.Height / 2;
     image.Mutate(x => x.Resize(width, height));
     http.Response.Headers.CacheControl = $"public,max-age={TimeSpan.FromHours(24).TotalSeconds}";
-    // ObjectDisposedException: Cannot access a disposed object. Object name: 'Image`1'.
+
     return Results.Stream(stream => image.SaveAsync(stream, PngFormat.Instance), "image/png");
+    // when using is used in first line, ie using var image = await Image.LoadAsync("wwwroot/front.png");
+    // ObjectDisposedException: Cannot access a disposed object. Object name: 'Image`1'.
 });
 
 app.Run();
